1efb1d9d72b8167aecd9e81b046089b6d6a36868
Kir <kir.kuznetsov2013@gmail.com>
2022-09-13 22:02:14 +0300

    feat: Created new class for file analysis

---
+++ data_analysis.py
@@ -0,0 +1,71 @@
+from pathlib import Path
+import os
+
+
+def get_data_from_file(file_name: str) -> list[list]:
+    data = []
+    with open(Path(os.path.realpath(__file__)).parent / file_name) as file:
+        lines = file.readlines()
+
+        for line in lines:
+            data.append([item for item in line.replace('\n', '').split(",")])
+    return data
+
+
+class DataAnalyser:
+    data = []
+
+    def __init__(self, data: list[list] = []):
+        self.data = data
+
+    def get_header(self) -> list[str]:
+        return self.data[0]
+
+    # Can return columns as well
+    def __getitem__(self, key: int | str) -> list:
+        if type(key) is str:
+            index = self.data[0].index(key)
+            return [item[index] for item in self.data[1:]]
+        else:
+            return self.data[1:][key]
+
+    def find_float_max_min(self, key: int | str, ignore_zeros: bool = False) -> (float, float):
+        arr = [float(item) for item in self[key] if float(item) != 0 or not ignore_zeros]
+        return max(arr), min(arr)
+
+    def get_unique_items(self, index: int | str) -> list:
+        return list(set(self[index]))
+
+    def sort_by_key(self, key_col: str, value_col: str, only_unique: bool = True) -> dict[list]:
+        result = {}
+        key_index = self.data[0].index(key_col)
+        val_col_index = self.data[0].index(value_col)
+        for line in self.data[1:]:
+            if line[key_index] not in result.keys():
+                result[line[key_index]] = []
+            if line[val_col_index] not in result[line[key_index]] or not only_unique:
+                result[line[key_index]].append(line[val_col_index])
+        return result
+
+
+
+# --------------------------------
+# Examples
+# --------------------------------
+
+analyser = DataAnalyser(get_data_from_file("processed_cars.csv"))
+
+for item in analyser.get_header():
+    print(item)
+
+print(analyser.get_unique_items("Country"))
+print(analyser.find_float_max_min("Year"))
+print(analyser.find_float_max_min("Mileage"))
+print(analyser.find_float_max_min("Maximum_power", True))
+print(analyser.find_float_max_min("Maximum_speed", True))
+print(analyser.find_float_max_min("Consumption", True))
+
+print("Models")
+for key, value in analyser.sort_by_key("mark", "Model").items():
+    print(key + ": ", value)
+
