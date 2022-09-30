1bf0bb9f3bd093cf06af0764bd4cf834d20ae01e
DeltaMansion <whatever>
2022-09-30 12:42:50 +0300

    feat: добавить нормализацию датасета и выделить набор данных для визуализации регрессии

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Текущее состояние:<br>
 Regression.py - работающая модель сети без настройки параметров, есть вывод графика обучения. Точность предикта случайная (400000-700000 в сравнении с 400000).<br>
 cooked-nn - загрузка кое-как обученной модели и подключение GUI интерфейса из cooked-interface. Тестовая версия V1. Сеть плохо работает с null данными.<br><br>
-Библиотеки: numpy, matplotlib, keras, PySimpleGUI.<br>
+Библиотеки: numpy, matplotlib, keras, PySimpleGUI, scikit learn.<br>
 Набор данных: https://www.kaggle.com/datasets/beaver68/cars-dataset-in-russia
 Используются столбцы: mark,  Model,  Mileage,  Year,  Car_class,  Maximum_power,  Drive_unit,  Box,  Boost_type,  Volume,  Torque,  Maximum_speed,  Speed_to_100,  Consumption,  [Price] 
--- Regression.py
+++ Regression.py
@@ -5,23 +5,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 import timeit
 import keys_lists
+from sklearn import preprocessing
 
 # вытащить данные из файла и разделить на наборы: X - то, что вводим в нейронку, Y - то, что хотим получить из неё
 def get_xy_data(file_name):
     dataset = np.loadtxt('./' + file_name, delimiter=';', skiprows=1, dtype='U')
+    EXAMPLES_FOR_TEST = 50
 
     # [:] - всё содержимое строки, [N] - номер столбца данных в файле
-    x = keys_lists.conversion_to_numbers_data(dataset[:, 0:14])
-    y = np.array(dataset[:, 14]).astype(float)
+    x_train = keys_lists.conversion_to_numbers_data(dataset[EXAMPLES_FOR_TEST:, 0:14])
+    y_train = np.array(dataset[EXAMPLES_FOR_TEST:, 14]).astype(float)
+
+    x_test = keys_lists.conversion_to_numbers_data(dataset[0:EXAMPLES_FOR_TEST, 0:14])
+    y_test = np.array(dataset[0:EXAMPLES_FOR_TEST, 14]).astype(float)
+    
+    # нормализация данных только для набора обучения X
+    # для тестирования и предиктов нормализация ломает весь результат 
+    for i in range(0, 14):
+        x_train[i] = preprocessing.normalize([x_train[i]])
 
     #если надо посмотреть на данные
     #print(x)
     #print(y)
 
-    return x, y
+    return x_train, y_train, x_test, y_test
 
 # данные для обучения и тестирования
-x_train, y_train = get_xy_data("dataset.csv")
+x_train, y_train, x_test, y_test = get_xy_data("dataset.csv")
 columns_counter = len(x_train[0])
 
 # создание модели сети
@@ -39,6 +49,11 @@ start_seconds: float = timeit.default_timer()
 es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, min_delta=10000000000, patience=5) # если захочется проверить раннюю остановку обучения
 history = model.fit(x_train, y_train, epochs=50, verbose=2, validation_split=0.2, batch_size=256) #callbacks=[es]
 end_seconds: float = timeit.default_timer()
+
+# график регрессии: x - y_test, y - y_pred
+y_pred = model.predict(x_test)
+for x, y in zip(y_test, y_pred):
+    print(x, y)
 
 # тестирование на пользовательских данных
 #final_test_data = np.array(['Chrysler', 'Sebring', '250000.0', '2007.0', 'D', '189.0', 'front', 'automatic', 'no', '2.7', '260.0', '207.0', '9.2', '10.7']) # 400000
